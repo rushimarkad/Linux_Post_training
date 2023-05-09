61a1879ef8aa49f9df958da0709795e209fec759
rushimarkad <rushikesh.markad@afourtech.com>
2023-05-09 12:18:49 +0530

    Added further changes

--- ps1.sh
+++ ps1.sh
@@ -3,26 +3,29 @@
 # Added the configuration file containing variables
 source config.cfg
 
+#Print timestamp for log
+date +"%d-%m-%Y %H:%M:%S" >> /home/rushikesh/post_training/ps.log
+
 user_mail_exists(){
 	  user_exists=$(cat /etc/passwd | grep "mail")
           if [ ! -z "$user_exists" ]; then
-            echo "The mail user and group exists."
+            echo "The mail user and group exists." >> /home/rushikesh/post_training/ps.log
             # Create the new empty file
             touch "$FILEPATH"
 
             # Change the owner and group of the file to mail:mail
             chown mail:mail "$FILEPATH"
 
-            echo "The owner and group of /app/access.log have been changed to mail:mail."
+            echo "The owner and group of /app/access.log have been changed to mail:mail." >> /home/rushikesh/post_training/ps.log
           else
-            echo "The mail user or group does not exist."
+            echo "The mail user or group does not exist." >> /home/rushikesh/post_training/ps.log
           fi
 }
 
 # Check if the file exists
 if [ ! -f "$FILEPATH" ]
 then
-    echo "$FILEPATH does not exists"
+    echo "$FILEPATH does not exists.....Exiting with status 55" >> /home/rushikesh/post_training/ps.log
     exit 55
 else
     # To get the filesize in MB
@@ -31,10 +34,10 @@ else
     if (( filesize >= 5 )); then
       rm "$FILEPATH"
       # Email address to send the email to
-          echo "$DEL_MESSAGE" | mail -s "$DEL_SUBJECT" "$TO_ADDRESS"
+          echo "$DEL_MESSAGE" | mail -s "$DEL_SUBJECT" "$TO_ADDRESS" >> /home/rushikesh/post_training/ps.log
 
           # Print a message indicating that the mail has been sent
-          echo "Mail has been sent to $TO_ADDRESS with subject $DEL_SUBJECT"
+          echo "Mail has been sent to $TO_ADDRESS with subject $DEL_SUBJECT" >> /home/rushikesh/post_training/ps.log
 
          # To check if the user and group mail exists
           user_mail_exists
@@ -47,13 +50,13 @@ else
           mv "$FILEPATH" "/app/$NEWNAME"
 
           # Print a message indicating that the file has been renamed
-          echo "File renamed to $NEWNAME"
+          echo "File renamed to $NEWNAME" >> /home/rushikesh/post_training/ps.log
 
          # Email address to send the email to
-           echo "$RENAME_MESSAGE" | mail -s "$RENAME_SUBJECT" "$TO_ADDRESS"
+           echo "$RENAME_MESSAGE" | mail -s "$RENAME_SUBJECT" "$TO_ADDRESS" >> /home/rushikesh/post_training/ps.log
 
           # Print a message indicating that the mail has been sent
-          echo "Mail has been sent to $TO_ADDRESS with subject $RENAME_SUBJECT"
+          echo "Mail has been sent to $TO_ADDRESS with subject $RENAME_SUBJECT" >> /home/rushikesh/post_training/ps.log
 
           # To check if the user and group mail exists
           user_mail_exists
--- ps3.sh
+++ ps3.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+#Print timestamp for log
+date +"%d-%m-%Y %H:%M:%S" >> /home/rushikesh/post_training/ps.log
+
 #Take the variable from config file
 source config.cfg
 
 # set time zone to US Eastern time
 export TZ="America/Los_Angeles"
-echo "Timezone changed to America/Los_Angeles successfully"
+echo "Timezone changed to America/Los_Angeles successfully" >> /home/rushikesh/post_training/ps.log
 
 # Delete old log files and write the paths to a log file
 find "$FOLDER" -name "*.log" -type f -mtime +$AGE_THRESHOLD -delete -print > /home/rushikesh/deleted_files.log
@@ -13,11 +16,11 @@ if [[ -s /home/rushikesh/deleted_files.log ]]; then
     # Send email with deleted file list as attachment
     DELETED_FILES_SUBJECT="Deleted file list $(date +'%Y-%m-%d %H:%M:%S')"
     echo -e "Please find attached the list of deleted files.\n\n" | mailx -s "$DELETED_FILES_SUBJECT" -A /home/rushikesh/deleted_files.log -- rushikesh.markad@afourtech.com
-    echo "Following files are deleted successfully"
-    cat /home/rushikesh/deleted_files.log
-    echo "mail sent with list of deleted files"
+    echo "Following files are deleted successfully" >> /home/rushikesh/post_training/ps.log
+    cat /home/rushikesh/deleted_files.log >> /home/rushikesh/post_training/ps.log
+    echo "mail sent with list of deleted files" >> /home/rushikesh/post_training/ps.log
 else
-    echo "None of the file deleted"
+    echo "None of the file deleted" >> /home/rushikesh/post_training/ps.log
 fi
 
 
@@ -28,12 +31,13 @@ if [[ ! -z "$COMPRESSED_FILES" ]]; then
     # Send email with compressed file list as email body
     COMPRESSED_FILES_SUBJECT="Compressed file list $(date +'%Y-%m-%d %H:%M:%S')"
     echo -e "Please find below the list of compressed files:\n\n$COMPRESSED_FILES" | mailx -s "$COMPRESSED_FILES_SUBJECT" rushikesh.markad@afourtech.com
-    echo "Following files are compressed successfully : $COMPRESSED_FILES"
-    echo "mail sent with list of compressed files"
+    echo "Following files are compressed successfully : $COMPRESSED_FILES" >> /home/rushikesh/post_training/ps.log
+    echo "mail sent with list of compressed files" >> /home/rushikesh/post_training/ps.log
 else
-    echo "None of the file is compressed"
+    echo "None of the file is compressed" >> /home/rushikesh/post_training/ps.log
 fi
 
 
 # set time zone again to IST
 export TZ="Asia/Kolkata"
+echo "Timezone changed to Asia/Kolkata successfully" >> /home/rushikesh/post_training/ps.log
