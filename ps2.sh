c468a7c5f3f8cba49b98875568990d3ef2c53be5
rushimarkad <rushikesh.markad@afourtech.com>
2023-05-17 09:45:35 +0530

    Half done

---
+++ ps2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+#Take the variable from config file
+source variable.ini
+
+function log_statement () {
+  echo "$(date "+%d-%m-%Y %H:%M:%S") : $1" >> "$LOGPATH"
+}
+
+# Create the log files
+create_log_files() {
+  for ((i=1; i<=5; i++))
+  do
+    log_file="$LOG_DIR/log$i.log"
+    touch "$log_file"
+    echo "Log File $i" > "$log_file"
+  done
+}
+
+# Function to generate random text lines and append them to log files
+generate_random_logs() {
+  while true
+  do
+    for ((i=1; i<=5; i++))
+    do
+      log_file="$LOG_DIR/log$i.log"
+      random_log=$(shuf -n 1 -e "ERROR: This is an error message" "WARNINGS: This is a warning message" "INFO: This is an informational message")
+      echo "$random_log" >> "$log_file"
+    done
+    LOOP_PID=$$
+    echo "$LOOP_ID"
+    #bg $LOOP_PID
+    sleep 120
+  done
+}
+
+# Create the log files
+create_log_files
+
+# Call the function to generate random lines and append them to log files
+generate_random_logs
+
+echo "Process in backround"
