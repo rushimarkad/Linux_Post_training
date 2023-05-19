65390f8bd6bc7314650643c32ef8c5c1c1f4fe56
rushimarkad <rushikesh.markad@afourtech.com>
2023-05-19 10:14:33 +0530

    Commit msg

--- ps2.sh
+++ ps2.sh
@@ -15,8 +15,7 @@ create_log_files() {
   do
     log_file="$LOG_DIR/log$i.log"
     touch "$log_file"
-    echo "Log File $i" > "$log_file"
-  done
+  done 
 }
 
 # Function to generate random text lines and append them to log files
@@ -29,8 +28,26 @@ generate_random_logs() {
       random_log=$(shuf -n 1 -e "ERROR: This is an error log" "WARNINGS: This is a warning log" "INFO: This is an informational message")
       echo "$random_log" >> "$log_file"
     done
-    sleep 120
+    sleep 30 # For demo
+    # sleep 120		#actual
   done &
+}
+
+EMAIL_SUBJECT="Log files with ERROR or WARNINGS"
+EMAIL_BODY=""
+
+# Function to send email with log file details
+function send_email {
+  # Update the email body with the new logs files
+  EMAIL_BODY=""
+  while read LINE
+  do
+    EMAIL_BODY+="$LINE\n"
+  done < <(find $LOG_DIR -name "*.log" -type f -mmin -60 -exec grep -Hn "ERROR\|WARNINGS" {} \; | sed 's/:[^:]*:/: line /g')
+
+  # Send the email with the details of the log files
+  echo -e "$EMAIL_BODY" | mail -s "$EMAIL_SUBJECT" "$TO_ADDRESS"
+  log_statement "Email has been sent to $TO_ADDRESS with the files containing ERRORS/WARNINGS"
 }
 
 # Create the log files
@@ -39,4 +56,14 @@ create_log_files
 # Call the function to generate random lines and append them to log files
 generate_random_logs
 
-echo "Process in backround"
+new_logs=$(find $LOG_DIR -name "*.log" -type f -mmin -60 -exec grep -Hn "ERROR\|WARNINGS" {} \;) 
+echo "$new_logs" 
+
+# Call the function to send mails in proper format
+while true
+do
+  if [ $(date +%M) = "00" ]; then
+    send_email 
+  fi
+  sleep 60
+done
